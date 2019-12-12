106dbc59e1bee364d12a653926d4fbca6a3081ce
vikingGame <czyz.jakub@op.pl>
2019-12-12 18:03:24 +0100

    ważne

--- not_not/lib/main.dart
+++ not_not/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 //mój komentarz
+//super ważne rzeczy norma
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
